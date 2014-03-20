d25de889cde7f77d1c9d5dcf995431901c56f822
broekhuizen <rudibroekhuizen@yahoo.com>
2014-03-20 15:17:26 +0100

    Modifications

--- manifests/init.pp
+++ manifests/init.pp
@@ -39,45 +39,53 @@
 #https://launchpad.net/~bas-dikkenberg/+archive/burp-stable
 
 class burp (
- $server = "server",
- $directory = "/mnt/backup/burpdata",
- $max_children = "25",
- $max_status_children = "25",
- $keep = "60",
- $waittime = "20",
- $starttime = "Mon,Tue,Wed,Thu,Fri,Sat,Sun,00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23",
- ){
+# general settings
+  $mode                = "server",
+  
+# server settings for /etc/burp-server.conf 
+  $directory           = "/mnt/backup/burpdata",
+  $max_children        = "25",
+  $max_status_children = "25",
+  $keep                = "60",
+  $waittime            = "20",
+  $starttime           = "Mon,Tue,Wed,Thu,Fri,Sat,Sun,00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23",
+ 
+# server settings for client config files in /etc/clientconfdir
+  $clientconf_hash     = {'NNMS00' => { $clientname        = $name,
+                                        $includes          = undef,
+                                        $excludes          = "D:/$RECYCLE.BIN/",
+                                        $options           = undef,
+                                        $password          = undef,
+                                      }
+                         }
+ # client settings
+  $installpackage = true,
+  ) {
+  
+  case $::mode {
+    server: {
+      # Create file burp-server.conf
+      file { '/etc/burp/burp-server.conf':
+        ensure  => present,
+        mode    => '600',
+        content => template("burp/burp-server.conf.erb"),
+        require => Package['burp']
+      }
 
-  if $::operatingsystem != 'Ubuntu' {
-    fail('This module only works on Ubuntu')
-  }
+      # Start service
+      service { 'burp':
+        ensure  => 'running',
+        require => File['/etc/burp/burp-server.conf'] 
+      }
 
-#Install package burp
-  file { '/etc/apt/sources.list.d':
-    ensure => 'directory',
-  }
-
-  apt::ppa { 'ppa:bas-dikkenberg/burp-latest':
-    require => File['/etc/apt/sources.list.d']
-  }
-
-  package { 'burp':
-    ensure => '1.4.10-1ubuntu2',
-    require => Apt::Ppa['ppa:bas-dikkenberg/burp-latest']
-  }
-
-#Create file burp-server.conf
-  file { '/etc/burp/burp-server.conf':
-    ensure  => present,
-    mode    => '600',
-    content => template("burp/burp-server.conf.erb"),
-    require => Package['burp']
-  }
-
-#Start service
-  service { 'burp':
-    ensure  => 'running',
-    require => File['/etc/burp/burp-server.conf'] 
+      # Fill /etc/burp/clientconfdir
+      create_resources('burp::clientconf', $clientconf_hash)
+        
+    client: {
+      if $installpackage {
+      class { 'burp::installpackage' }
+      }
+    }
   }
 
 }
