6c3ddae6633092538778123b77d4f953288673e8
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2014-04-10 08:47:43 +0000

    updated pre backup script functionality

--- manifests/init.pp
+++ manifests/init.pp
@@ -59,7 +59,7 @@ class burp (
   $max_children        = "25",
   $max_status_children = "25",
   $keep                = "60",
-  $waittime            = "20",
+  $waittime            = "20h",
   $starttime           = "Mon,Tue,Wed,Thu,Fri,Sat,Sun,00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23",
   $common_clientconfig = ['working_dir_recovery_method=resume'],
 # server: create client config files in /etc/clientconfdir for Windows clients
