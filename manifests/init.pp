9424913187d601a96343ce1affa1c46e927b3a0b
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2014-06-03 12:03:06 +0000

    fixed bug in clientconfig for empty excludes

--- manifests/init.pp
+++ manifests/init.pp
@@ -58,7 +58,7 @@ class burp (
   $directory           = "/mnt/backup/burpdata",
   $max_children        = "25",
   $max_status_children = "25",
-  $keep                = "60",
+  $keep                = "100",
   $waittime            = "20h",
   $starttime           = "Mon,Tue,Wed,Thu,Fri,Sat,Sun,00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23",
   $common_clientconfig = ['working_dir_recovery_method=resume'],
