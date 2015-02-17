c1d55449e4a8977c40e00b86f81992690ce249a2
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-02-17 13:59:31 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -55,13 +55,15 @@ class burp (
   $cron     = true,
 
 # server: settings for /etc/burp-server.conf
-  $directory           = "/mnt/backup/burpdata",
-  $max_children        = "25",
-  $max_status_children = "25",
-  $keep                = "100",
-  $waittime            = "20h",
-  $starttime           = "Mon,Tue,Wed,Thu,Fri,Sat,Sun,00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23",
-  $common_clientconfig = ['working_dir_recovery_method=resume'],
+  $directory             = "/mnt/backup/burpdata",
+  $max_children          = "25",
+  $max_status_children   = "25",
+  $keep                  = "100",
+  $waittime              = "20h",
+  $starttime             = "Mon,Tue,Wed,Thu,Fri,Sat,Sun,00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23",
+  $common_clientconfig   = ['working_dir_recovery_method=resume'],
+  $server_script_post    = "/etc/burp/server_script_post",
+  $backup_stats_logstash = "true",
 
 # server: create client config files in /etc/clientconfdir for Windows clients
   $clientconf_hash     = { 'servername-01.domain' => { includes => ['C:/', 'D:/'],
